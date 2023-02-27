7f3812738b156abbfb09b5502318254b47399eba
TriForMine <quentin@triformine.dev>
2023-02-27 12:04:34 +0100

    fix exercice2-3

--- tp2/exercice2-3.py
+++ tp2/exercice2-3.py
@@ -1,21 +1,26 @@
 import os, signal, sys
+
+
 def CHLD_handler(signal, ignore):
     """Traitant de l'interruption SIGCHLD"""
-    pid, status = os.wait()
     while True:
         try:
             pid, status = os.waitpid(-1, os.WNOHANG)
             if pid == 0:
                 break
             if os.WIFEXITED(status):
-                print("**père** child %d terminated normally with exit status=%d" % (pid, os.WEXITSTATUS(status)))
+                print(
+                    "**père** child %d terminated normally with exit status=%d"
+                    % (pid, os.WEXITSTATUS(status))
+                )
             else:
                 print("**père** child %d terminated abnormally" % pid)
         except:
             break
 
+
 # Programme principal
-if __name__ == '__main__':
+if __name__ == "__main__":
     signal.signal(signal.SIGCHLD, CHLD_handler)  # Installe le traitant
     for i in range(10):  # Création des fils
         pid = os.fork()
@@ -34,7 +39,7 @@ if __name__ == '__main__':
     # Astuce: Pour savoir si la saisie a été interrompue, et donc savoir
     # s'il faut recommencer (avec une boucle while) il suffit de regarder
     # si la variable de saisie existe.
-    while not 'saisie' in globals():
+    while not "saisie" in globals():
         try:
             saisie = input("**père** tapez quelque chose... ")
         except:  # La réception d'un signal provoque l'interruption de la saisie
